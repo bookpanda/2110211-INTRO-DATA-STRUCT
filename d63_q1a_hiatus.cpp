1785d0a2af61340528b6f7b25ff1be2989c91649
Idhibhat Pankam <idhibhat.pankam@agoda.com>
2023-08-24 15:29:44 +0700

    update

--- d63_q1a_hiatus.cpp
+++ d63_q1a_hiatus.cpp
@@ -3,6 +3,7 @@
 using namespace std;
 
 int main() {
+    std::ios_base::sync_with_stdio(false); std::cin.tie(0);
     int n,m;
     vector<pair<int,int>> v;
 
@@ -25,15 +26,17 @@ int main() {
     for(int i=0;i<m;i++) {
         int year, month;
         cin >> year >> month;
+//        cout << "ans : " << endl;
         pair<int,int> p = {year, month};
-        auto it = lower_bound(v.begin(), v.end(), p);
-        if(it != v.end()) {
+        auto it = upper_bound(v.begin(), v.end(), p);
+        it--;
+        if(it >= v.begin()) {
             if(it->first == year && it->second == month)
-                cout << "0 0" << endl;
+                cout << "0 0" << "\n";
             else
-                cout << it->first << " " << it->second << endl;
+                cout << it->first << " " << it->second << "\n";
         } else {
-            cout << "-1 -1" << endl;
+            cout << "-1 -1" << "\n";
         }
     }
 
--- d63_qx_inside_interval.cpp
+++ d63_qx_inside_interval.cpp
@@ -3,6 +3,7 @@
 using namespace std;
 
 int main() {
+    std::ios_base::sync_with_stdio(false); std::cin.tie(0);
     int n,m;
     vector<pair<int,int>> v;
     vector<int> va;
@@ -19,26 +20,36 @@ int main() {
         va.push_back(x.first);
         va.push_back(x.second);
 //        cout << x.first << x.second;
-        type[x.first] = 1;
-        type[x.second] = -1;
+        if(x.first == x.second)
+            type[x.first] = 2;
+        else {
+            type[x.first] = 1;
+            type[x.second] = -1;
+        }
     }
-    auto comp = [](pair<int,int> &p, int value) {
-        return p.first < value;
-    };
+//    auto comp = [](pair<int,int> &p, int value) {
+//        return p.first < value;
+//    };
     for(int i=0;i<m;i++) {
         int a;
         cin >> a;
 //        auto it = lower_bound(v.begin(), v.end(), a, comp);
         auto it = lower_bound(va.begin(), va.end(), a);
 //        cout << "it = " << it->first << " " << it->second << endl;
-        cout << "it = " <<  *it << endl;
+//        cout << "it = " <<  *it << "| type = " << type[*it] << endl;
         if(it == va.end()) {
             cout << 0 << " ";
         }
         else if(*it > a) {
-            cout << 0 << " ";
-        } else {
-            cout << 1 << " ";
+            if(type[*it] == -1)
+                cout << 1 << " ";
+            else if(type[*it] == 1 || type[*it] == 2)
+                cout << 0 << " ";
+        } else if(*it == a) {
+//            if(type[*it] == -1)
+                cout << 1 << " ";
+//            else if(type[*it] == 1)
+//                cout << 0 << " ";
         }
 //        if(type[*it] == -1 || (type[*it] ==1 && *it == a)) {
 //        } else {
