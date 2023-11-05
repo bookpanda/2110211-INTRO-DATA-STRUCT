6d130a04c4977964e14aee4a8e3d0291fbc27421
Idhibhat Pankam <marginpankam@gmail.com>
2023-11-05 16:30:49 +0700

    e

--- d65_q2b_some_move/student.h
+++ d65_q2b_some_move/student.h
@@ -9,42 +9,35 @@
 template <typename T>
 void CP::vector_some_move<T>::insert(int index, std::vector<T> &value) {
   // Your code here
-  auto it = std::lower_bound(aux.begin(), aux.end(), index);
-  int i = it - aux.begin();
-  // if(index == 0) {
-  //   mData.insert(mData.begin(), value);
-
-  // } else if(index == mSize) {
-  //   mData.push_back(value);
-  // } else
-  if(i < 1800) {//new vector
-    int prev = i > 0 ? aux[i-1] : 0;
-    int pos = index - prev;
-    int sz = mData[i].size();
-    int afterpos = sz-pos;
-    aux[i] -= afterpos;
-    mData.insert(mData.begin() + i+1, value);
-    aux.insert(aux.begin() + i+1, aux[i]);
-    std::vector<T> temp;
-    for(int a=pos;a<mData[i].size();a++) {
-      temp.push_back(mData[i][a]);
+  auto it = std::lower_bound(aux.begin(), aux.end(), index);//aux=10 -> idx=9
+  int auxid = it - aux.begin();
+  if(mData[auxid].size() > 2000) { //new vector
+    int st = auxid>0 ? index - aux[auxid-1] : index;
+    std::vector<T> tmp;
+    for(int i=st;i<mData[auxid].size();i++) {
+      tmp.push_back(mData[auxid][i]);
     }
-    mData.insert(mData.begin() + i+2, temp);
-    aux.insert(aux.begin() + i+2, aux[i]+afterpos);
-    mData[i].erase(mData[i].begin() + pos, mData[i].end());
-    i++;
-  } else {//current vector
-    int prev = i > 0 ? aux[i-1] : 0;
-    int pos = index - prev;
-    mData[i].insert(mData[i].begin() + pos, value.begin(), value.end());
+    mData.insert(mData.begin() + auxid+1, tmp);
+    aux.insert(aux.begin() + auxid+1, aux[auxid]);
+    
+    aux[auxid] -= mData[auxid].size() - st;
+    mData[auxid].erase(mData[auxid].begin() + st, mData[auxid].end());
+
+    mData.insert(mData.begin() + auxid+1, value);
+    aux.insert(aux.begin() + auxid+1, aux[auxid]);
+
+  } else { //use current vector
+    int st = auxid>0 ? index - aux[auxid-1] : index;
+    mData[auxid].insert(mData[auxid].begin() + st, value.begin(), value.end());
+    aux[auxid] += value.size();
   }
 
-  while(i<aux.size()) {
+  for(int i=auxid+1;i<aux.size();i++) {
     aux[i] += value.size();
-    i++;
   }
-  mCap+=value.size();
-  mSize+=value.size();
+
+  mSize += value.size();
+  mCap += value.size();
 }
 
 #endif
